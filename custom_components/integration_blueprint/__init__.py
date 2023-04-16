2dc8dbeeec32d7e225e031ed1830c4f5f2deb038
fars-fede-fire <87006332+fars-fede-fire@users.noreply.github.com>
2023-04-16 05:06:40 +0000

    Initial commit

--- custom_components/integration_blueprint/__init__.py
+++ custom_components/integration_blueprint/__init__.py
@@ -1,7 +1,7 @@
-"""Custom integration to integrate integration_blueprint with Home Assistant.
+"""Custom integration to integrate price_calc with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/fars-fede-fire/price_calc
 """
 from __future__ import annotations
 
